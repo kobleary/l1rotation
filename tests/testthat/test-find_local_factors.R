b0dc734949561786864871a095bcf94747932646
Kobler <C1RAK01@rb.win.frb.org>
2025-03-12 14:36:44 -0400

    #3 add test w rotation of true lambda + noise as initial lambda

--- tests/testthat/test-find_local_factors.R
+++ tests/testthat/test-find_local_factors.R
@@ -1,4 +1,19 @@
 
+max_cosine_similarity <- function(est_mat, true_mat){
+  colMaxs(abs(t(est_mat) %*% true_mat)/nrow(true_mat))
+}
+
+orthonormalize <- function(matrix){
+  r <- ncol(matrix)
+  n <- nrow(matrix)
+
+
+  orthonorm_matrix <- sqrt(n) * pracma::gramSchmidt(matrix)$Q
+
+  return(orthonorm_matrix)
+
+}
+
 test_that("find_local_factors() returns same result with same seed with and without Lambda0 argument, small example", {
   X <- load_matrix(testthat::test_path("fixtures", "single_realization.csv"))
   r <- 2
@@ -76,6 +91,28 @@ test_that("find_local_factors() returns same result with same seed with and with
 
 
 })
+
+
+test_that("find_local_factors() returns matrix similar to truth using a orthonormal rotation of the true Lambda", {
+
+  i <- 10
+
+  X <- load_matrix(testthat::test_path("fixtures", stringr::str_glue("X_{i}.csv")))
+  true_lambda <- load_matrix(testthat::test_path("fixtures", stringr::str_glue("true_lambda_{i}.csv")))
+  e <- matrix(rnorm(n * r, sd = 0.1), nrow = n, ncol = r)
+
+  true_lambda_rotated_plus_noise <- orthonormalize(true_lambda + e)
+
+  M <- nrow(X)
+  n <- ncol(X)
+  r <- 4
+
+  lambda_rotated_true_plus_noise <- find_local_factors(X, r, true_lambda_rotated_plus_noise)$Lambda_rotated
+  cos_sim_true_plus_noise <- max_cosine_similarity(lambda_rotated_true_plus_noise, true_lambda)[-1]
+  expect_true(all(cos_sim_true_plus_noise > 0.98))
+
+})
+
 
 
 
