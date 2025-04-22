85303671f65440c839fec20e41b562a2e86d00f7
Kobler <C1RAK01@rb.win.frb.org>
2025-04-22 11:35:08 -0400

    update links to data, better macro ex desc

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
-#' Example data with four factors from the replication files of Freyaldenhoven et. al (2023).
+#' Example data with four factors from the replication files of \href{https://simonfreyaldenhoven.github.io/papers/factor_rotation.pdf}{Freyaldenhoven et. al (2025)}.
 #'
 #' @format ## `example_data`
 #' A matrix with 224 rows and 207 columns.
 #'
-#' @source Dataset available as a .mat file can be found under local_factors.zip <https://simonfreyaldenhoven.github.io/software/>
+#' @source Dataset available as a .mat file can be generated from local_factors.zip <https://simonfreyaldenhoven.github.io/software/>
 "example_data"
