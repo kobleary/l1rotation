7221245f4cc0652a255e536bce7b3cd13e351dc7
Kobler <C1RAK01@rb.win.frb.org>
2025-05-07 17:31:56 -0400

    create nonorthogonoal illustration, combine obj fn and sim data

--- vignettes/simulate_data.R
+++ vignettes/simulate_data.R
@@ -1,78 +1,68 @@
-simulate_data_2d <- function(T, m1, m2, n, rho_t, rho_i, lambda_option, error_magnifier = 0.3) {
-  # rho_t, rho_i correlation in error terms.
-  
-  # Dep defines the structure of the factors.
-  Dep <- matrix(c(1, m1, n + 1 - m2, n), ncol = 2, byrow = TRUE)
-  dim <- dim(Dep)
-  r <- dim[1]
-  F <- matrix(rnorm(T * r), nrow = T) %*% chol(matrix(c(1, 0.3, 0.3, 1), nrow = 2)) # Cholesky decomposition
-  Lambda <- matrix(0, n, r)
-  
-  if (lambda_option == 1) {
-    for (k in 1:r) {
-      Lambda[Dep[k, 1]:Dep[k, 2], k] <- rep(1, Dep[k, 2] - Dep[k, 1] + 1) + rnorm(Dep[k, 2] - Dep[k, 1] + 1)
-    }
-  } else if (lambda_option == 2) {
-    for (k in 1:r) {
-      Lambda[Dep[k, 1]:Dep[k, 2], k] <- runif(Dep[k, 2] - Dep[k, 1] + 1, 0.1, 1.9)
-    }
-  } else if (lambda_option == 3) {
-    for (k in 1:r) {
-      Lambda[Dep[k, 1]:Dep[k, 2], k] <- rnorm(Dep[k, 2] - Dep[k, 1] + 1)
-    }
-  }
-  
-  # Create noise
-  epsilon <- matrix(rnorm(T * n), nrow = T)
-  cross_dep <- numeric(n)
-  time_dep <- numeric(T)
-  
-  for (i in 1:n) {
-    cross_dep[i] <- rho_i^(i - 1)
-  }
-  
-  for (t in 1:T) {
-    time_dep[t] <- rho_t^(t - 1)
-  }
-  
-  A <- toeplitz(time_dep)
-  B <- toeplitz(cross_dep)
-  e <- sqrt(A) %*% epsilon %*% sqrt(B)
-  
-  # Now create X
-  X <- F %*% t(Lambda) + error_magnifier * e
-  
-  # Also save normalized loading matrix
-  truth_normal <- Lambda
-  for (k in 1:r) {
-    truth_normal[, k] <- sqrt(n) * Lambda[, k] / sqrt(sum(Lambda[, k]^2)) # normalize by columns to unit length
-  }
-  
-  return(list(X = X, Lambda = Lambda, truth_normal = truth_normal, F = F, r = r))
-}
+# Note: must be run from "vignettes" folder
 
-# Constants -------
+library(dplyr)
+source("helper_functions.R")
+
+# Simple simulation -------
 set.seed(200)
-
-scaling=1
-m1 = 1; 
-m2 = 2; 
-lambda_option=2; 
 T <- 224
 n <- 3
-rho_t <- 0.3
-rho_i <- 0.1
-factorno <- 2
+r <- 2
+rho <- .4
+primitive <- matrix(rnorm(T * 2), nrow = T, ncol = 2)
 
-simulated_data <- simulate_data_2d(T, m1, m2, n, rho_t, rho_i, lambda_option, error_magnifier = 0.3)
+factor_cov <- matrix(c(1, rho, 0, sqrt(1-rho^2)), byrow = TRUE, nrow = 2)
+F <- primitive %*% factor_cov
+Lambda <- matrix(c(1, .5, 1, 0, 0, -1), ncol = 2, byrow = TRUE)
+e <- matrix(rnorm(T * n), nrow = T, ncol = n)
+X <- F %*% t(Lambda) + 0.5 * e
 
-lf <- l1rotation::local_factors(simulated_data$X, r = 2)
+lf <- l1rotation::local_factors(X, r = 2)
 
-simulated_data$truth_normal[,2] = simulated_data$truth_normal[,2] * -1
 
-write.csv(simulated_data$X, "data.csv", row.names = FALSE)
-write.csv(simulated_data$truth_normal, "truth_normal.csv", row.names = FALSE)
+r <- 2
+no_draws <- 2000
+Lambda0 <- lf$initial_loadings
+
+# Construct objective function ---------
+# Create starting points for algorithm
+initial_draws <- matrix(stats::rnorm(r * no_draws), nrow = r)
+initial_draws <- normalize(initial_draws, p = 2)
+
+theta <- cartesian_to_spherical(initial_draws)
+
+slider_input <- seq(-3.13, 3.13, by = .01)
+
+
+y <- purrr::map_dbl(
+  slider_input,
+  \(x) objectivefcn_spherical(x, Lambda0)
+)
+
+
+obj_function_data <- tibble::tibble(theta = slider_input, y = y) %>%
+  mutate(
+    dist1 = abs(theta - round(2*pi,2)),
+    dist2 = abs(theta - round(pi, 2)/2),
+  ) %>%
+  mutate(min_dist1 = dist1 == min(dist1),
+         min_dist2 = dist2 == min(dist2),
+         highlight = min_dist1 | min_dist2
+  )
+
+obj_function_data %>%
+  ggplot(aes(theta, y)) + geom_line() +
+  geom_point(
+    data = filter(obj_function_data, highlight == TRUE),
+    aes(theta, y)
+    )
+
+# Write all to csv in "vignettes" folder ----
+write.csv(X, "data.csv", row.names = FALSE)
+write.csv(Lambda, "truth_normal.csv", row.names = FALSE)
 write.csv(lf$initial_loadings, "lambda0.csv", row.names = FALSE)
+write.csv(obj_function_data, "obj_function_data.csv")
+
 
 
 
